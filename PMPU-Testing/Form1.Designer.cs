76af595fa08db927253e7564de4c385741e9e8d0
readdawg <42706385+readdawg@users.noreply.github.com>
2018-10-14 19:58:43 -0500

    Started coding buttons
    Coded button calls for stored procedures.

--- PMPU-Testing/Form1.Designer.cs
+++ PMPU-Testing/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_single = new System.Windows.Forms.Button();
+            this.btn_changeMonitor = new System.Windows.Forms.Button();
+            this.btn_quad = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_single
+            // 
+            this.btn_single.Location = new System.Drawing.Point(97, 70);
+            this.btn_single.Name = "btn_single";
+            this.btn_single.Size = new System.Drawing.Size(138, 23);
+            this.btn_single.TabIndex = 0;
+            this.btn_single.Text = "Single Camera";
+            this.btn_single.UseVisualStyleBackColor = true;
+            this.btn_single.Click += new System.EventHandler(this.btn_single_Click);
+            // 
+            // btn_changeMonitor
+            // 
+            this.btn_changeMonitor.Location = new System.Drawing.Point(97, 128);
+            this.btn_changeMonitor.Name = "btn_changeMonitor";
+            this.btn_changeMonitor.Size = new System.Drawing.Size(138, 23);
+            this.btn_changeMonitor.TabIndex = 1;
+            this.btn_changeMonitor.Text = "Change Single Monitor";
+            this.btn_changeMonitor.UseVisualStyleBackColor = true;
+            this.btn_changeMonitor.Click += new System.EventHandler(this.btn_changeMonitor_Click);
+            // 
+            // btn_quad
+            // 
+            this.btn_quad.Location = new System.Drawing.Point(97, 99);
+            this.btn_quad.Name = "btn_quad";
+            this.btn_quad.Size = new System.Drawing.Size(138, 23);
+            this.btn_quad.TabIndex = 2;
+            this.btn_quad.Text = "Quad Cameras";
+            this.btn_quad.UseVisualStyleBackColor = true;
+            this.btn_quad.Click += new System.EventHandler(this.btn_quad_Click);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(97, 158);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(138, 23);
+            this.btn_send.TabIndex = 3;
+            this.btn_send.Text = "Send Change";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.btn_quad);
+            this.Controls.Add(this.btn_changeMonitor);
+            this.Controls.Add(this.btn_single);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_single;
+        private System.Windows.Forms.Button btn_changeMonitor;
+        private System.Windows.Forms.Button btn_quad;
+        private System.Windows.Forms.Button btn_send;
     }
 }
 
--- PMPU-Testing/Form1.cs
+++ PMPU-Testing/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,72 @@ namespace PMPU_Testing
 {
     public partial class Form1 : Form
     {
+        //Initialize data table
+        DataTable dt1 = new DataTable();
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Hard set DataConnectionString
+            PMPU_Testing.Properties.Settings.Default.DataConnectioString = @"Data Source=localhost\MSSQL2012;Initial Catalog=InsightEnt;User ID=sa;Password=V4in$ight";
+
+            //Clear and create DataTable
+            dt1.Clear();
+            dt1.Columns.Add("Monitor");
+            dt1.Columns.Add("Type");
+            dt1.Columns.Add("Class");
+        }
+
+        private void btn_single_Click(object sender, EventArgs e)
+        {
+            //Set Data Row
+            dt1.Clear();
+            DataRow _single = dt1.NewRow();
+            _single["Monitor"] = "1";
+            _single["Type"] = "0";
+            _single["Class"] = "";
+        }
+
+        private void btn_quad_Click(object sender, EventArgs e)
+        {
+            dt1.Clear();
+            DataRow _quad = dt1.NewRow();
+            _quad["Monitor"] = "1";
+            _quad["Type"] = "4";
+            _quad["Class"] = "";
+        }
+
+        private void btn_changeMonitor_Click(object sender, EventArgs e)
+        {
+            dt1.Clear();
+            DataRow _quadChange = dt1.NewRow();
+            _quadChange["Monitor"] = "1";
+            _quadChange["Type"] = "4";
+            _quadChange["Class"] = "";
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            //Open connection
+            SqlConnection con = new SqlConnection(PMPU_Testing.Properties.Settings.Default.DataConnectioString);
+            con.Open();
+            SqlCommand sqlcmd = new SqlCommand("usp_", con);
+            sqlcmd.CommandType = CommandType.StoredProcedure;
+            sqlcmd.Parameters.AddWithValue("@VW_change", dt1);
+            sqlcmd.ExecuteNonQuery();
+            MessageBox.Show("Video Wall Change Push Complete");
+            con.Close();
+
+            //Close Video Wall
+
+
+            //Launch Video Wall
+
         }
     }
 }
